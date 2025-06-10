eb8c216b5b924b83011402fb779c1dd0248ad537
Dong Hyeon, Shin <109497684+dongkoony@users.noreply.github.com>
2025-06-10 14:02:43 +0900

    feat: VPC 출력값 수정 - VPC 및 서브넷 관련 리소스 ID와 CIDR 블록을 k8s 리소스로 변경

--- infra/terraform/modules/vpc/outputs.tf
+++ infra/terraform/modules/vpc/outputs.tf
@@ -1,39 +1,39 @@
 output "vpc_id" {
   description = "생성된 VPC의 ID"
-  value       = aws_vpc.main.id
+  value       = aws_vpc.k8s_vpc.id
 }
 
 output "vpc_cidr" {
   description = "VPC의 CIDR 블록"
-  value       = aws_vpc.main.cidr_block
+  value       = aws_vpc.k8s_vpc.cidr_block
 }
 
 output "public_subnet_id" {
   description = "퍼블릭 서브넷 ID"
-  value       = aws_subnet.public.id
+  value       = aws_subnet.public_subnet.id
 }
 
 output "public_subnet_cidr" {
   description = "퍼블릭 서브넷 CIDR 블록"
-  value       = aws_subnet.public.cidr_block
+  value       = aws_subnet.public_subnet.cidr_block
 }
 
 output "private_subnet_id" {
   description = "프라이빗 서브넷 ID"
-  value       = aws_subnet.private.id
+  value       = aws_subnet.private_subnet.id
 }
 
 output "private_subnet_cidr" {
   description = "프라이빗 서브넷 CIDR 블록"
-  value       = aws_subnet.private.cidr_block
+  value       = aws_subnet.private_subnet.cidr_block
 }
 
 output "nat_gateway_id" {
   description = "NAT 게이트웨이 ID"
-  value       = var.enable_nat_gateway ? aws_nat_gateway.main[0].id : null
+  value       = aws_nat_gateway.k8s_nat.id
 }
 
 output "internet_gateway_id" {
   description = "인터넷 게이트웨이 ID"
-  value       = aws_internet_gateway.main.id
+  value       = aws_internet_gateway.k8s_igw.id
 }
