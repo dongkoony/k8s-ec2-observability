efe520cffdf659d6b81997e27b38d62d7cdc616d
Dong Hyeon, Shin <109497684+dongkoony@users.noreply.github.com>
2025-06-10 17:40:57 +0900

    feat: EC2 인스턴스 SSH 키 이름 설정 개선 - null 체크 로직 수정

--- infra/terraform/modules/ec2-worker/main.tf
+++ infra/terraform/modules/ec2-worker/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "worker" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-  key_name      = var.ssh_key_name
+  key_name      = var.ssh_key_name != null ? var.ssh_key_name : null
 
   vpc_security_group_ids = [aws_security_group.worker.id]
 
